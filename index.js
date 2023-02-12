4ed0d86fa9ea46919a42b931ef3edbf2b020c9f0
Qluqlu3 <plksmcgof@gmail.com>
2023-02-12 17:28:44 +0900

    fix

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-const axios = require('axios');
+const https = require('https');
 
 //OpenWeatherMap API
 const API_KEY = 'YOUR_APP_ID';
-async function getWeather(city) {
-  const endpoint = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=YOUR_APP_ID`;
 
-  try {
-    const response = await axios.get(endpoint);
-    const weatherData = response.data;
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
 
-    console.log(`地名: ${weatherData.name}`);
-    console.log(`天気: ${weatherData.weather[0].description}`);
-    console.log(`温度: ${weatherData.main.temp}℃`);
-    console.log(`湿度: ${weatherData.main.humidity}%`);
-    console.log(`気圧: ${weatherData.main.pressure}hPa`);
-  } catch (error) {
-    console.error(error);
-  }
-}
+console.log('地名を入力:');
 
-getWeather('Tokyo,jp');
+process.stdin.on('data', function (cityName) {
+  const encodedCityName = encodeURIComponent(cityName.trim());
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodedCityName}&appid=${API_KEY}`;
+
+  https
+    .get(url, (res) => {
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        const weatherData = JSON.parse(data);
+        console.log(`${cityName}:`);
+        console.log(`気温: ${weatherData.main.temp}`);
+        console.log(`湿度: ${weatherData.main.humidity}%`);
+        console.log(`気圧: ${weatherData.main.pressure} hPa`);
+        console.log(`天気: ${weatherData.weather[0].description}`);
+      });
+    })
+    .on('error', (error) => {
+      console.error(`${error.message}`);
+    });
+});
